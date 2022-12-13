f714ca34a61d899c8bebe59822b18cc97ac8a488
Shu Li <108698293+Cheulie@users.noreply.github.com>
2022-12-13 23:01:06 +0000

    Wheels finally loading

--- build-a-car/src/components/Car.js
+++ build-a-car/src/components/Car.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import Wheel from "./Wheel.js"
 
 export default function Car(props) {
-  return <h2>Hi, I am a Car! I have {props.numberOfWheels} wheels</h2>;
+  return (
+  <>
+   <h2>Hi, I am a Car! I have {props.numberOfWheels} wheels</h2>
+   <Wheel />
+   </>
+  );
 }
 
 
